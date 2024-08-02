962a455b78598a67dbefc6505ebe8b64c2d7d9d0
pecodigos <pedrenriquecunha@gmail.com>
2024-08-02 06:01:41 -0300

    insert implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -31,5 +32,11 @@ public class Main {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n===== TEST 3 : seller findAll =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
     }
 }
