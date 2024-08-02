acef2c925de5a0134de3a9cc08a96cf1deca9514
pecodigos <pedrenriquecunha@gmail.com>
2024-08-02 06:14:29 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -88,6 +88,21 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public void deleteById(Integer id) {
 
+        PreparedStatement st = null;
+
+        try {
+            st = connection.prepareStatement("DELETE FROM seller WHERE id = ?");
+
+            st.setInt(1, id);
+
+            st.executeUpdate();
+        }
+        catch(SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
